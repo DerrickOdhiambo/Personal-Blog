b5b925fb0533a3244b98ff43c25c566630c5bf30
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-28 08:50:27 +0300

    create routes for login and registration of users

---
+++ blog/users/views.py
@@ -0,0 +1,21 @@
+from flask import render_template, flash, url_for, redirect
+from blog.users.forms import LoginForm, RegistrationForm
+from . import users
+
+
+@users.route('/registration', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account for {form.username.data} has been created successfully!', 'success')
+    return redirect(url_for('main.home'))
+  return render_template('register.html', title = 'Register', form = form)
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    return redirect(url_for('main.home'))
+  return render_template('login.html', title = 'Login', form = form)
+
