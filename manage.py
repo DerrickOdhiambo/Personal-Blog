238dff6d199eb51e6fbaa5434dc8be142de5ce32
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-28 08:40:07 +0300

    initialized the database and migrate

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
-from blog import create_app
+from blog import create_app,db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from blog.models import User, Post
 
 
 #creating app instance
 app = create_app('development')
 
+migrate = Migrate(app, db)
 manager =  Manager(app)
+
+manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+  return dict(app = app, db = db, User = User, Post = Post)
 
 if __name__=='__main__':
   manager.run()
