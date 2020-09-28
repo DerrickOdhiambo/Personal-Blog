a1194e2050e2d34244a7cf64dccbf665d56702c0
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-28 17:29:30 +0300

    register the posts package as a blueprint

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
 def create_app(config_name):
   #initializing app
@@ -14,12 +21,16 @@ def create_app(config_name):
 
   #initializing extensions
   db.init_app(app)
+  bcrypt.init_app(app)
+  login_manager.init_app(app)
 
   #registering a blueprint
   from .main import main as main_blueprint
   from .users import users as users_blueprint
+  from .posts import posts as posts_blueprint
 
   app.register_blueprint(main_blueprint)
   app.register_blueprint(users_blueprint)
+  app.register_blueprint(posts_blueprint)
 
   return app
