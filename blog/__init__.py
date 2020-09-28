364f5b225046b4953e20a81281711eb4fb3cbe3b
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-28 08:47:51 +0300

    initialize SQLAlchemy database

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+
+
+db = SQLAlchemy()
 
 def create_app(config_name):
   #initializing app
@@ -8,8 +12,14 @@ def create_app(config_name):
   #application configurations
   app.config.from_object(config_options[config_name])
 
+  #initializing extensions
+  db.init_app(app)
+
   #registering a blueprint
   from .main import main as main_blueprint
+  from .users import users as users_blueprint
+
   app.register_blueprint(main_blueprint)
+  app.register_blueprint(users_blueprint)
 
   return app
