2f3b9f9475eafbef2668c4287b9ad88b36f6a744
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-28 08:44:51 +0300

    add the about and articles routes

--- blog/main/views.py
+++ blog/main/views.py
@@ -3,4 +3,12 @@ from . import main
 
 @main.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', title = 'Home')
+
+@main.route('/about')
+def about():
+  return render_template('about.html', title = 'About')
+
+@main.route('/articles')
+def articles():
+  return render_template('articles.html', title = 'Articles')
